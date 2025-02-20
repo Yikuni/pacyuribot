fad99b5845273b30a8fc95b14fd278d35aa2836b
yikuni <yikuni@qq.com>
2025-02-20 11:59:14 +0800

    fix: 初始化文件夹逻辑问题

--- initialize/router.go
+++ initialize/router.go
@@ -1,37 +1,31 @@
 package initialize
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
 	"pacyuribot/global"
 	"pacyuribot/logger"
 	"pacyuribot/middleware"
 	"pacyuribot/router"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Routers() *gin.Engine {
 	r := gin.Default()
 
 	// 保证./data/crawl_data文件夹存在
-	_, err := os.Stat("./data/crawl_data")
-	if os.IsNotExist(err) {
-		// 如果不存在，创建文件夹
-		err = os.MkdirAll("./data/crawl_data", os.ModePerm)
-		if err != nil {
-			logger.Error("Failed to mkdir ./data/crawl_data: %s", err.Error())
-			panic(err)
-		}
+	err := os.MkdirAll("./data/crawl_data", os.ModePerm)
+	if err != nil {
+		logger.Error("Failed to mkdir ./data/crawl_data: %s", err.Error())
+		panic(err)
 	}
+
 	// 保证cache文件夹存在
-	_, err = os.Stat("cache")
-	if os.IsNotExist(err) {
-		// 如果不存在，创建文件夹
-		err = os.Mkdir("cache", os.ModePerm)
+	err = os.Mkdir("cache", os.ModePerm)
+	if err != nil {
 		logger.Error("Failed to mkdir ./cache: %s", err.Error())
-		if err != nil {
-			panic(err)
-		}
+		panic(err)
 	}
 	r.Static("/cache", "./cache")
 	r.Static("/data/crawl_data", "./data/crawl_data")
